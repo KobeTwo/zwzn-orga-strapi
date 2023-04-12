0fef70ea86effc6659fe5d07dcfcb6aefb04f3f1
Robert Schmidt <r.schmidt@dotsource.de>
2023-04-12 18:44:57 +0200

    create a discord channel after event creation

---
+++ src/api/event/content-types/event/lifecycles.ts
@@ -0,0 +1,26 @@
+import { Client, TextChannel, ClientOptions, GatewayIntentBits, ChannelType } from 'discord.js';
+
+const clientOptions: ClientOptions = {
+    intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages],
+  };
+
+const client = new Client(clientOptions);
+client.login(process.env.DISCORD_BOT_TOKEN);
+
+export default {
+    afterCreate(event) {
+        const { result } = event;
+        console.log('after create: ' + JSON.stringify(result))
+        const channelNamePre = result.startDate + ' ' + result.type + ' ' + result.oponent
+        const channelName = channelNamePre.replace(/\W+/g, '-').toLowerCase();
+        const guild = client.guilds.cache.get(process.env.DISCORD_GUILD_ID);
+        if (!guild) return console.error('Unable to find guild.');
+        guild.channels.create({
+            name: channelName,
+            type: ChannelType.GuildText,
+            topic: 'a description',
+        })
+        .then((channel: TextChannel) => console.log(`Created channel ${channel.name}`))
+        .catch(console.error);
+    },
+};
