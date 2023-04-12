cf0ecdae1416f991bc5add8c97979b31830ca922
Robert Schmidt <r.schmidt@dotsource.de>
2023-04-12 17:34:40 +0200

    added event response data type

---
+++ src/api/event-response/routes/event-response.ts
@@ -0,0 +1,7 @@
+/**
+ * event-response router
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreRouter('api::event-response.event-response');
---
+++ src/api/event-response/services/event-response.ts
@@ -0,0 +1,7 @@
+/**
+ * event-response service
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreService('api::event-response.event-response');
