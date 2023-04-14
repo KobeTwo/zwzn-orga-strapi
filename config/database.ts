9aed7ac440f4846c9a5f2f7bda87ee84e5ca90ed
Robert Schmidt <r.schmidt@dotsource.de>
2023-04-14 14:38:37 +0200

    test

--- config/database.ts
+++ config/database.ts
@@ -1,7 +1,10 @@
 import path from 'path';
 
 export default ({ env }) => {
+  const parse = require("pg-connection-string").parse;
+  const config = parse(process.env.DATABASE_URL);
   const client = env('DATABASE_CLIENT', 'sqlite');
+  const isDevelopment = process.env.NODE_ENV === 'development'
 
   const connections = {
     mysql: {
@@ -29,22 +32,12 @@ export default ({ env }) => {
     postgres: {
       connection: {
         connectionString: env('DATABASE_URL'),
-        host: env('DATABASE_HOST', 'localhost'),
-        port: env.int('DATABASE_PORT', 3306),
-        database: env('DATABASE_NAME', 'strapi'),
-        user: env('DATABASE_USERNAME', 'strapi'),
-        password: env('DATABASE_PASSWORD', 'strapi'),
-        ssl: env.bool('DATABASE_SSL', false) && {
-          key: env('DATABASE_SSL_KEY', undefined),
-          cert: env('DATABASE_SSL_CERT', undefined),
-          ca: env('DATABASE_SSL_CA', undefined),
-          capath: env('DATABASE_SSL_CAPATH', undefined),
-          cipher: env('DATABASE_SSL_CIPHER', undefined),
-          rejectUnauthorized: env.bool(
-            'DATABASE_SSL_REJECT_UNAUTHORIZED',
-            true
-          ),
-        },
+        host: config.host,
+        port: config.port,
+        database: config.database,
+        user: config.user,
+        password: config.password,
+        ssl: false,
         schema: env('DATABASE_SCHEMA', 'public'),
       },
       pool: { min: env.int('DATABASE_POOL_MIN', 2), max: env.int('DATABASE_POOL_MAX', 10) },
@@ -61,12 +54,21 @@ export default ({ env }) => {
       useNullAsDefault: true,
     },
   };
-
-  return {
-    connection: {
-      client,
-      ...connections[client],
-      acquireConnectionTimeout: env.int('DATABASE_CONNECTION_TIMEOUT', 60000),
-    },
-  };
+  if(isDevelopment){
+    return {
+      connection: {
+        client,
+        ...connections['sqllite'],
+        acquireConnectionTimeout: env.int('DATABASE_CONNECTION_TIMEOUT', 60000),
+      },
+    };
+  }else{
+    return {
+      connection: {
+        client,
+        ...connections['postgres'],
+        acquireConnectionTimeout: env.int('DATABASE_CONNECTION_TIMEOUT', 60000),
+      },
+  }
+  
 };
